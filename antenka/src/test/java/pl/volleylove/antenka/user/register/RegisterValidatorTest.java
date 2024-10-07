462e94c6992df85df38a5c593184736f24c41265
Malgorzata Galat <m.galat@rocketmail.com>
2024-10-07 21:39:51 +0200

    added unit tests for RegisterValidator

--- antenka/src/test/java/pl/volleylove/antenka/user/register/RegisterValidatorTest.java
+++ antenka/src/test/java/pl/volleylove/antenka/user/register/RegisterValidatorTest.java
@@ -15,7 +15,6 @@ public class RegisterValidatorTest {
     public void isAgeCorrectTestPastDates(int age, boolean expected) {
 
         //current requirements - min: 16, max: 150
-
         Assert.assertEquals(expected, RegisterValidator.isAgeCorrect(LocalDate.now().minusYears(age)));
 
     }
@@ -29,12 +28,46 @@ public class RegisterValidatorTest {
     }
 
     @Test
-    public void isAgeCorrectTestBirthdayIsNull() {
+    @Parameters({"abc, abc, true", "ab1%343#, abd, false" , "dddadse, !^abc4, false", "ab, ab, false", "ab, ab, false", "abc, abc, true",
+    "abcabcabcabcabcabcab, abcabcabcabcabcabcab, true", ", , false"})
+    public void isNameFormatCorrect(String firstName, String lastName, boolean expected) {
 
-        Assert.assertFalse(RegisterValidator.isAgeCorrect(null));
+        Assert.assertEquals(expected, RegisterValidator.isNameFormatCorrect(firstName, lastName));
+
+    }
+
+    @Test
+    @Parameters({"abc, false", "gosiawp.pl, false", "gosia@wp.pl, true", "%$#gosia@wp.pl, false", "gosia@wppl, false",
+            "gosia123@wp.pl, true", " , false", "aschshshenansnfnesnrnenjklodasqwerthgjfmikmgkpgfds@wp.pl, true", //max length of a local part - 50
+            "aschshshenansnfnesnrnenjklodasqwerthgjfmikmgkpgfdsaaaaaaaaaa@wp.pl, false"}) //length of a local part is over the limit
+    public void isEmailFormatCorrect(String email, boolean expected) {
+
+        Assert.assertEquals(expected, RegisterValidator.isEmailFormatCorrect(email));
+
     }
 
 
+    @Test
+    @Parameters({"abc, false", "ab!abd, false", "dsad!sdsdsdas, false", ",false", "dfadw!4dsa, true",
+            "!4#@#$@$@!#$!@$, false", "!3fdjklpolfgjklomngtrhbgnmlkopf, false"}) //too long
+    public void isPasswordFormatCorrect(String password, boolean expected) {
 
+        Assert.assertEquals(expected, RegisterValidator.isPasswordFormatCorrect(password));
+
+    }
+
+    @Test
+    public void registerValidatorMethodsGetNull() {
+        Assert.assertFalse(RegisterValidator.isAgeCorrect(null));
+
+        Assert.assertFalse(RegisterValidator.isNameFormatCorrect(null, "surname"));
+        Assert.assertFalse(RegisterValidator.isNameFormatCorrect("firstname", null));
+        Assert.assertFalse(RegisterValidator.isNameFormatCorrect(null, null));
+
+        Assert.assertFalse(RegisterValidator.isEmailFormatCorrect(null));
+
+        Assert.assertFalse(RegisterValidator.isPasswordFormatCorrect(null));
+
+    }
 
 }
