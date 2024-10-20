52e44d69f02312c0c024a627ae741f4bee9cee34
Malgorzata Galat <m.galat@rocketmail.com>
2024-10-20 20:17:24 +0200

    added equals() and hashcode() implementation for this class

--- antenka/src/main/java/pl/volleylove/antenka/user/register/RegisterResponse.java
+++ antenka/src/main/java/pl/volleylove/antenka/user/register/RegisterResponse.java
@@ -2,15 +2,30 @@ package pl.volleylove.antenka.user.register;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 import pl.volleylove.antenka.enums.RegisterInfo;
 
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Builder
+@ToString
 public class RegisterResponse {
 
     private String email;
     private List<RegisterInfo> registerInfo;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegisterResponse that = (RegisterResponse) o;
+        return Objects.equals(email, that.email) && Objects.equals(registerInfo, that.registerInfo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, registerInfo);
+    }
 }
