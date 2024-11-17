2137a5e468af96289cd2b41819cb4c5777ec0473
Malgorzata Galat <m.galat@rocketmail.com>
2024-11-17 22:13:00 +0100

    removed unnecessary annotation

--- antenka/src/main/java/pl/volleylove/antenka/security/JwtProperties.java
+++ antenka/src/main/java/pl/volleylove/antenka/security/JwtProperties.java
@@ -3,11 +3,9 @@ package pl.volleylove.antenka.security;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-@Configuration
 @ConfigurationProperties("security.jwt") //configuration is in the application.properties file and starts with security.jwt
 public class JwtProperties {
 
