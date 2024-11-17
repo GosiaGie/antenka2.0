6413e93cc60f34d1e591e585a2b2b677494e2cc0
Malgorzata Galat <m.galat@rocketmail.com>
2024-11-17 22:11:20 +0100

    changed variable to final

--- antenka/src/main/java/pl/volleylove/antenka/map/GoogleMapsApiHelper.java
+++ antenka/src/main/java/pl/volleylove/antenka/map/GoogleMapsApiHelper.java
@@ -9,15 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Setter
 public class GoogleMapsApiHelper {
 
-    private final GoogleMapsApiProperties googleMapsApiProperties;
+    private GoogleMapsApiProperties googleMapsApiProperties;
 
     @Autowired
     public GoogleMapsApiHelper(GoogleMapsApiProperties googleMapsApiProperties) {
         this.googleMapsApiProperties = googleMapsApiProperties;
     }
 
-    public String createURI(String street, String number, String zipCode, String locality){
-
+    public String createURI(String street, String number, String zipCode, String locality) {
         return googleMapsApiProperties.getApiPath()+"json?address="+ street + "+" + number
                 + "+" + zipCode + "+" +locality + "&key=" + googleMapsApiProperties.getApiKey();
     }
