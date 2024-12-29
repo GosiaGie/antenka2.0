a0080f6564cb8acfb525c84f9793324ff4d114a8
Malgorzata Galat <m.galat@rocketmail.com>
2024-12-29 13:54:03 +0100

    implemented equals() and hashcode()

--- antenka/src/main/java/pl/volleylove/antenka/event/match/signup/SignUpForMatchResponse.java
+++ antenka/src/main/java/pl/volleylove/antenka/event/match/signup/SignUpForMatchResponse.java
@@ -1,17 +1,19 @@
 package pl.volleylove.antenka.event.match.signup;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import pl.volleylove.antenka.entity.Match;
 import pl.volleylove.antenka.entity.Slot;
 import pl.volleylove.antenka.enums.SignUpInfo;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class SignUpForMatchResponse {
 
     private SignUpInfo info;
@@ -19,4 +21,16 @@ public class SignUpForMatchResponse {
     private Match match;
     private Slot slot;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SignUpForMatchResponse that = (SignUpForMatchResponse) o;
+        return info == that.info && Objects.equals(match, that.match) && Objects.equals(slot, that.slot);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(info, match, slot);
+    }
 }
