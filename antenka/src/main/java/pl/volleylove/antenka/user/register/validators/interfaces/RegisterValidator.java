597abd2111bab6d18cbd306e0acc1b358d0989a0
Malgorzata Galat <m.galat@rocketmail.com>
2025-11-11 21:22:21 +0100

    removed unnecessary imports

--- antenka/src/main/java/pl/volleylove/antenka/user/register/validators/interfaces/RegisterValidator.java
+++ antenka/src/main/java/pl/volleylove/antenka/user/register/validators/interfaces/RegisterValidator.java
@@ -1,9 +1,6 @@
 package pl.volleylove.antenka.user.register.validators.interfaces;
 
-import pl.volleylove.antenka.user.register.RegisterRequest;
-
 import java.time.LocalDate;
-import java.util.List;
 
 public interface RegisterValidator {
     boolean isAgeCorrect(LocalDate birthday);
